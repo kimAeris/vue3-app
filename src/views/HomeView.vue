22208ed5cc3485fa16058b3ba3a5f3f7bc567c92
KimAeris <kar2125@naver.com>
2024-03-06 10:53:24 +0900

    vuetify 설정

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HelloWorld />
+  <v-combobox
+    label="Combobox"
+    :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+  ></v-combobox>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld
+  }
+})
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
-
+import vuetify from 'vite-plugin-vuetify'
 import { defineConfig } from 'vite'
+
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@api': fileURLToPath(new URL('./src/api', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url))
     }
   }
 })
