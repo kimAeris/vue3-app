6f2bc3574e233a383bbd7946b0bfd4665ff3289d
KimAeris <kar2125@naver.com>
2024-03-06 13:31:37 +0900

    Axios 기본 세팅

--- README.md
+++ README.md
@@ -2,14 +2,6 @@
 
 This template should help get you started developing with Vue 3 in Vite.
 
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
 ## Project Setup
 
 ```sh
@@ -32,4 +24,40 @@ npm run build
 
 ```sh
 npm run lint
+```
+
+## Prettier
+
+```
+{
+  "$schema": "https://json.schemastore.org/prettierrc",
+  "arrowParens": "avoid", // 화살표 함수 괄호 사용 방식
+  "semi": true,
+  "bracketSameLine": true, // 객체 리터럴에서 괄호에 공백 삽입 여부
+  "bracketSpacing": false,
+  "tabWidth": 2,
+  "singleQuote": true, // single 쿼테이션 사용 여부
+  "printWidth": 100,
+  "trailingComma": "all" // 여러 줄을 사용할 때, 후행 콤마 사용 방식
+}
+```
+
+> 옵션 참고  
+> https://persistent-weed-dev.tistory.com/entry/%EC%BD%94%EB%93%9C%EB%A5%BC-%EB%8D%94-%EC%9D%B4%EC%81%98%EA%B2%8C-Prettier
+
+```
+
+## Axios
+
+### withCredentials
+
+withCredentials 옵션은 단어의 의미에서 알 수 있듯이, 서로 다른 도메인(크로스 도메인)에 요청을 보낼 때 요청에 credential 정보를 담아서 보낼 지를 결정하는 항목입니다.
+
+다른 도메인에 있는 백엔드 서버와 통신할 때 쿠키와 인증 정보를 함께 전송하는 것이다. 이를 통해 사용자 세션을 유지하고 보안이 필요한 요청을 처리할 수 있다.
+
+> 사용 방식
+>
+> 1.  일반적으로 쿠키를 사용하여 사용자 세션을 관리하는 경우, 클라이언트와 서버 간에 쿠키를 주고받아야 한다. 이 때, withCredentials를 사용하여 쿠키를 전송하면 사용자의 로그인 상태를 유지할 수 있다.
+> 2.  클라이언트와 서버가 다른 도메인에 위치하는 경우 (CORS 상황), 쿠키와 인증 정보를 함께 전송해야 하는 경우에 withCredentials를 사용한다. 이를 통해 다른 도메인의 서버와 안전하게 통신할 수 있다.
+> 3.  특정 API 요청이 인증이 필요한 경우, 쿠키에 저장된 토큰 또는 세션 정보를 사용하여 인증을 수행할 수 있다. 이 때 withCredentials를 사용하여 요청에 쿠키를 포함시킬 수 있다.
 ```
---
+++ src/api/index.js
@@ -0,0 +1,188 @@
+import axios from 'axios';
+import dayjs from 'dayjs';
+import router from '@/routes';
+import { dateFormatting } from '@/utils/days';
+
+const LAST_ACTION_KEY = 'lastAction';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const getMenuCode = () => {
+  const menuStore = useMenu();
+  return menuStore.currentPage?.menu_cd;
+};
+
+const getFeedback = (type, title, message, cancel, confirm) => {
+  const { openFeedback } = useFeedback();
+  openFeedback(type, title, message, cancel, confirm);
+};
+
+const refreshStore = () => {
+  const menuStore = userMenu();
+  const userStore = useUser();
+
+  userStore.user = {};
+  userStore.shop = {};
+  menuStore.menus = [];
+  menuStore.mainMEnu = {};
+  menuStore.currentPage = {};
+};
+
+const create = (baseUrl, options) => {
+  const instance = axios.create({
+    baseUrl,
+    ...options,
+    widthCredentials: true,
+  });
+
+  instance.interceptors.request.use(
+    async req => {
+      try {
+        const userStore = useUser();
+
+        const accessToken = userStore.accessToken;
+
+        const expireTime = dayjs(localStorage.getItem(LAST_ACTION_KEY)).add(
+          1,
+          'hour',
+        );
+
+        // 헤더에 토큰 추가
+        req.headers.Authorization = accessToken;
+
+        // 로그인세션 만료 검증
+        if (
+          // import.meta.env.DEV : 앱이 개발 환경에서 실행 중인지 여부
+          // 날짜가 지정한 시간 단위에서 특정 날짜보다 이후 인지, 이후 이거나 같은지 구분
+          !import.meta.env.DEV &&
+          accessToken &&
+          dayjs().isAfter(expireTime)
+        ) {
+          const res = await axios.post(
+            `${BASE_URL}/logout`,
+            {
+              user_id: userStore.user.user_id,
+            },
+            {
+              headers: {
+                Authorization: accessToken,
+              },
+              withCredentials: true,
+            },
+          );
+
+          localStorage.removeItem(LAST_ACTION_KEY);
+          refreshStore();
+
+          router.push({ name: 'Login' });
+
+          getFeedback(
+            'error',
+            '로그인 만료',
+            '로그인이 만료되었습니다',
+            '확인',
+          );
+
+          return res;
+        }
+
+        // 메뉴코드 추가
+        if (
+          req.url !== '/system/S3/upload' &&
+          req.url !== '/system/S3/download'
+        ) {
+          const menuCode = getMenuCode();
+          if (menuCode) {
+            if (req.method === 'get') {
+              if (typeof req.params === 'string') {
+                req.params = JSON.parse(req.params);
+              }
+
+              req.params = {
+                ...req.params,
+                menu_cd: menuCode,
+              };
+            }
+          } else if (req.method === 'post') {
+            if (typeof req.data === 'string') {
+              req.data = JSON.parse(req.data);
+            }
+
+            req.data = {
+              ...req.data,
+              menu_cd: menuCode,
+            };
+          }
+        }
+
+        if (accessToken) {
+          localStorage.setItem(
+            LAST_ACTION_KEY,
+            dateFormatting(dayjs(), 'YYYY-MM-DD HH:mm:ss'),
+          );
+        }
+
+        return req;
+      } catch (error) {
+        if (import.meta.env.DEV) console.error(error);
+      }
+    },
+    error => Promise.reject(error),
+  );
+
+  instance.interceptors.response.use(
+    async res => {
+      if (res.data?.returnCode === 22) {
+        refreshStore();
+
+        router.push({ name: 'Login' });
+
+        getFeedback(
+          'error',
+          '중복 로그인',
+          '다른 곳에서 로그인 되었습니다',
+          '확인',
+        );
+
+        throw res.data.returnMessage;
+      }
+
+      return res;
+    },
+    async error => {
+      if (error.response.status === 401) {
+        try {
+          const {
+            data: { accessToken },
+          } = await instance.post(`${BASE_URL}/refreshToken`);
+
+          if (accessToken) {
+            const userStore = useUser();
+            userStore.accessToken = accessToken;
+            return await instance.request(error.config);
+          }
+        } catch (refreshError) {
+          // 개발 환경에서만 refreshError를 콘솔에 기록
+          if (import.meta.env.DEV) console.error(refreshError);
+          return Promise.reject(refreshError);
+        }
+      }
+
+      if (error.response.status === 403) {
+        refreshStore();
+
+        router.push({ name: 'Login' });
+
+        getFeedback('error', '권한 없음', '접근 권한이 없습니다', '확인');
+
+        return error;
+      }
+
+      return Promise.reject(error);
+    },
+  );
+
+  return instance;
+};
+
+export const common = create(`${BASE_URL}`);
